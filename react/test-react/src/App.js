2779206a3b7bc4fd76c376e9dddf71c74b431f0b
孙斌 <sunbin@sunbin-MacBook-Pro.local>
2021-07-12 13:48:24 +0800

    react 组件

--- react/test-react/src/App.js
+++ react/test-react/src/App.js
@@ -1,30 +1,38 @@
 import React from "react";
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      num : 0
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>当前计数：{this.state.num}</h2>
-        <button onClick={() => { this.increment()}}>+</button>
-        <button onClick={() => { this.decrement()}}>-</button>
-      </div>
-    )
-  }
-  increment() {
-    this.setState({
-      num:this.state.num + 1
-    })
-  }
-  decrement() {
-    this.setState({
-      num:this.state.num - 1
-    })
-  }
+//  没有this对象
+//  没有内部状态（hooks）
+export default function App() {
+  return (
+    <div>函数式组件</div>
+  )
 }
+
+// export default class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       num : 0
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>当前计数：{this.state.num}</h2>
+//         <button onClick={() => { this.increment()}}>+</button>
+//         <button onClick={() => { this.decrement()}}>-</button>
+//       </div>
+//     )
+//   }
+//   increment() {
+//     this.setState({
+//       num:this.state.num + 1
+//     })
+//   }
+//   decrement() {
+//     this.setState({
+//       num:this.state.num - 1
+//     })
+//   }
+// }
