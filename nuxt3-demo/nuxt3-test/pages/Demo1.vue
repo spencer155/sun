6ea9e0198d0b97d74ef5ba8e85a72bf9d9fbeffe
孙斌 <sunbin@sunbin-MacBook-Pro.local>
2022-04-11 15:48:57 +0800

    feat(nuxt3): 学习

---
+++ nuxt3-demo/nuxt3-test/pages/Demo1.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="">
+    <h1>Demo1 page</h1>
+    <NuxtLink to="/index">Index page</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style scoped></style>
---
+++ nuxt3-demo/nuxt3-test/pages/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="">
+    <h1>index page</h1>
+    <NuxtLink to="/demo1">Demo1 page</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+import {} from 'vue'
+</script>
+
+<style scoped></style>
